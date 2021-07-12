f7956f690163e59e8ea7cec712b8b3d202fa7cf1
Milan Martiniak <mino.martiniak@gmail.com>
2021-07-12 11:45:41 +0200

    SQL initializer

--- src/Services/Sample.Search/Function1.cs
+++ src/Services/Sample.Search/Function1.cs
@@ -1,4 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Indexes;
+using Azure.Search.Documents.Models;
+using Flurl.Http;
 using Kros.KORM;
 using Kros.KORM.Metadata.Attribute;
 using Microsoft.Azure.WebJobs;
@@ -10,7 +17,7 @@ namespace Sample.Search
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([TimerTrigger("0 * * * * *")] TimerInfo myTimer, ILogger log, ExecutionContext context)
+        public async static Task Run([TimerTrigger("0 * * * * *")] TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
             log.LogInformation($"Start search sync at: {DateTime.Now}.");
             var config = new ConfigurationBuilder()
@@ -22,28 +29,56 @@ namespace Sample.Search
             var connection = config["Catalog"];
             try
             {
-               using var database = Database.Builder.UseConnection(connection).Build();
+                using var database = Database.Builder.UseConnection(connection).Build();
 
-               foreach (var item in database.Query<Product>())
-               {
-                   log.LogInformation($"{item.Id}: {item.Name} - {item.Description} ({item.Price})");
-               }
+                foreach (var item in database.Query<Product>())
+                {
+                    log.LogInformation($"{item.Id}: {item.Name} - {item.Description})");
+                }
+
+                var a = await GetDocumentsAsync();
+                await UploadDocumentsAsync(database.Query<Product>());
             }
             catch
             {
             }
         }
 
+        private static async Task<string> GetDocumentsAsync()
+        {
+            string url = $"http://azsearch:8080/indexes/catalog/docs?api-version=2020-06-30-Preview&search=*";
+            string value = await url
+                .WithHeader("Content-Type", "application/json")
+                .GetStringAsync();
+
+            return value;
+        }
+
+        private static async Task UploadDocumentsAsync(IEnumerable<Product> documents)
+        {
+            var searchClient = CreateSearchIndexClient();
+            var batch = IndexDocumentsBatch.Upload(documents);
+
+            await searchClient.IndexDocumentsAsync(batch);
+        }
+
+        private static SearchClient CreateSearchIndexClient()
+        {
+            string searchServiceEndPoint = $"http://azsearch:8080";
+
+            var indexClient = new SearchIndexClient(new Uri(searchServiceEndPoint), new AzureKeyCredential("key"));
+
+            return indexClient.GetSearchClient("catalog");
+        }
+
         [Alias("Products")]
         public class Product
         {
-            public int Id { get; set; }
+            public string Id { get; set; }
 
             public string Name { get; set; }
 
             public string Description { get; set; }
-
-            public decimal Price { get; set; }
         }
     }
 }
